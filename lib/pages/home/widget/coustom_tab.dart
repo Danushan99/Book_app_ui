5c4bf2a01bcb634e576fc715f973cda3f7ee43f9
Danushan <it20051648@my.sliit.lk>
2022-01-04 11:04:17 +0530

    class

--- lib/pages/home/widget/coustom_tab.dart
+++ lib/pages/home/widget/coustom_tab.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class CoustomTab extends StatelessWidget {
+  const CoustomTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
