5f0eecc4abefc86cbf98b76cf3a9a72353172d53
Danushan <it20051648@my.sliit.lk>
2022-01-06 13:45:07 +0530

    book medel add

---
+++ lib/models/book.dart
@@ -0,0 +1,14 @@
+class book {
+  String type;
+  String name;
+  String publisher;
+  DateTime date;
+  String imageUrl;
+  num score;
+  num rating;
+  String review;
+  num hight;
+
+  book(this.type, this.name, this.publisher, this.date, this.imageUrl,
+      this.score, this.rating, this.review, this.hight);
+}
